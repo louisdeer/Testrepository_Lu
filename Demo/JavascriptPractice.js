ae70496ea329aab6e02be6647f603742fb66aefc
louisdeer <louisdeer@gmail.com>
2016-03-04 15:13:50 -0800

    this

---
+++ Demo/JavascriptPractice.js
@@ -0,0 +1,6 @@
+document.write("Starting to observe \"This\"\n\n\n\n\n");
+
+eval("this");
+function Fun_scope() {
+	eval("this");
+}
--- Demostrator/README.md
+++
@@ -1,2 +0,0 @@
-# phil_190_2016
-Class repository for Phil 190, Spring 2016
